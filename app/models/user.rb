bad29a408d56d77e3d875e13b65753e3cd16c1c2
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-09-12 11:06:55 -0600

    refactored serializer, more accurate score calculation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_many :plays
+
+  def get_total_score(game_id)
+    plays.where(game_id: game_id).sum(:score)
+  end
 end
--- spec/requests/api/v1/required_requests_spec.rb
+++ spec/requests/api/v1/required_requests_spec.rb
@@ -15,6 +15,20 @@ describe "GET /api/v1/games/1" do
     expect(game_display).to have_key(:scores)
     expect(game_display[:scores].first[:score]).to eq(15)
   end
+  it 'adds to score' do
+    user_1 = User.create(name: 'Jeb')
+    user_2 = User.create(name: 'George')
+    game = Game.create(player_1_id: user_1.id, player_2_id: user_2.id)
+    play_1 = user_1.plays.create(game_id: 1, word: 'fez', score: 15)
+    play_2 = user_2.plays.create(game_id: 1, word: 'zek', score: 16)
+
+    post "/api/v1/games/#{game.id}"
+    expect(response).to be_successful
+    game_display = JSON.parse(response.body, symbolize_names: true)
+    expect(game_display).to have_key(:game_id)
+    expect(game_display).to have_key(:scores)
+    expect(game_display[:scores].first[:score]).to eq(15)
+  end
 end
 
 # *Feature 2: API - Viewing Game Score*
