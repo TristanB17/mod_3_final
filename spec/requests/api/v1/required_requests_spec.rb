70e058a30e8bcd3e202dbfefba40f61efc6ea994
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-09-12 11:10:50 -0600

    refactored test

--- spec/requests/api/v1/required_requests_spec.rb
+++ spec/requests/api/v1/required_requests_spec.rb
@@ -18,39 +18,23 @@ describe "GET /api/v1/games/1" do
   it 'adds to score' do
     user_1 = User.create(name: 'Jeb')
     user_2 = User.create(name: 'George')
+    word = 'at'
     game = Game.create(player_1_id: user_1.id, player_2_id: user_2.id)
     play_1 = user_1.plays.create(game_id: 1, word: 'fez', score: 15)
     play_2 = user_2.plays.create(game_id: 1, word: 'zek', score: 16)
 
-    post "/api/v1/games/#{game.id}"
+    post "/api/v1/games/#{game.id}/plays?user_id=#{user_1.id}&word=#{word}"
+    expect(response).to be_successful
+    expect(response.status).to eq(201)
+
+    get "/api/v1/games/#{game.id}"
     expect(response).to be_successful
     game_display = JSON.parse(response.body, symbolize_names: true)
-    expect(game_display).to have_key(:game_id)
-    expect(game_display).to have_key(:scores)
-    expect(game_display[:scores].first[:score]).to eq(15)
+
+    expect(game_display[:scores].first[:score]).to eq(17)
   end
 end
 
-# *Feature 2: API - Viewing Game Score*
-#
-# Background: This story assumes the base data from running `rake db:seed`
-#
-# When I send a GET request to “/api/v1/games/1” I receive a JSON response as follows:
-#
-# ```{
-#   "game_id":1,
-#   "scores": [
-#     {
-#       "user_id":1,
-#       "score":15
-#     },
-#     {
-#       "user_id":2,
-#       "score":16
-#     }
-#   ]
-# }```
-#
 # *Feature 3: API - Posting a Play*
 #
 # Background: This story assumes the base data from running `rake db:seed`
